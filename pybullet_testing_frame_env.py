ba74027f96488f164669fe8ac1409b7b9a04b3aa
rubencg195 <rubencg195@hotmail.com>
2019-02-13 09:00:58 -0330

    videos and PPO

--- pybullet_testing_frame_env.py
+++ pybullet_testing_frame_env.py
@@ -25,6 +25,7 @@ class WalkingSpider(gym.Env):
 
     p.resetSimulation()
     p.setGravity(0,0,-10) # m/s^2
+    # p.setTimeStep(1./60.)   # sec
     p.setTimeStep(0.01)   # sec
     self.plane               = p.loadURDF("plane.urdf")
 
@@ -36,14 +37,14 @@ class WalkingSpider(gym.Env):
       self.cubeStartOrientation
     )
     self.movingJoints = [0, 2, 3, 5, 6, 8, 9, 11]
-    #paramId = p.addUserDebugParameter("My Param", 0, 100, 50)
+    # self.movingJoints = [0, 2]
+
   def _seed(self, seed=None):
     self.np_random, seed = seeding.np_random(seed)
     return [seed]
 
   def step(self, action):
     self.assign_throttle(action)
-    p.stepSimulation()
     self.observation    = self.compute_observation()
     reward              = self.compute_reward()
     done                = self.compute_done()
@@ -56,13 +57,11 @@ class WalkingSpider(gym.Env):
     self.maxV            = 8.72  #0.12sec/60 deg = 500 deg/s = 8.72 rad/s
     self.envStepCounter  = 0
     
-
     p.resetBasePositionAndOrientation(
       self.robotId,
       posObj=self.cubeStartPos,
       ornObj=self.cubeStartOrientation
     )
-    
     self.observation     = self.compute_observation()
     return np.array(self._observation)
     
@@ -78,11 +77,10 @@ class WalkingSpider(gym.Env):
         targetVelocity= target  
     )
   def assign_throttle(self, action):
-    deltav = 0.05
+    deltav = 1
     for i, key in enumerate(self.movingJoints) :
-      self.vt[i] = self.clamp(self.vt[i] + action[i], -1, 1)
+      self.vt[i] = self.clamp(self.vt[i] + action[i], -1.5, 1.5)
       self.moveLeg( robot=self.robotId, id=key,  target= self.vt[i] ) 
-    #print(self.vt)
 
   def clamp(self, n, minn, maxn):
     return max(min(maxn, n), minn)
@@ -95,28 +93,77 @@ class WalkingSpider(gym.Env):
     JointStates   = p.getJointStates(self.robotId, self.movingJoints ) 
     BaseAngVel    = p.getBaseVelocity(self.robotId)
     ContactPoints = p.getContactPoints(self.robotId, self.plane)
-    print("\nBase Orientation \nPos( x= {} , y = {} , z = {} )\nRot Quaternion( x = {} , y = {} , z = {}, w = {} )\n\n".format(
-      baseOri[0][0], baseOri[0][1], baseOri[0][2], 
-      baseOri[1][0], baseOri[1][1], baseOri[1][2], baseOri[1][3]
-    ))
+    
+    debug  = False
+    if (debug):
+      print("\nBase Orientation \nPos( x= {} , y = {} , z = {} )\nRot Quaternion( x = {} , y = {} , z = {}, w = {} )\n\n".format(
+        baseOri[0][0], baseOri[0][1], baseOri[0][2], 
+        baseOri[1][0], baseOri[1][1], baseOri[1][2], baseOri[1][3]
+      ))
+      print("\nJointStates: (Pos,Vel,6 Forces [Fx, Fy, Fz, Mx, My, Mz], appliedJointMotorTorque)\n")
+      for i, joint in enumerate(JointStates):
+        print( "Joint #{} State: Pos {}, Vel {} Fx {} Fy {} Fz {} Mx {} My {} Mz {}, ApliedJointTorque {} ".format(i, joint[0], joint[1], joint[2][0], joint[2][1], joint[2][2], joint[2][3], joint[2][4], joint[2][5], joint[3] ) )
+      print ("\nBase Angular Velocity (Linear Vel( x= {} , y= {} , z=  {} ) Algular Vel(wx= {} ,wy= {} ,wz= {} ) ".format( BaseAngVel[0][0], BaseAngVel[0][1], BaseAngVel[0][2] , BaseAngVel[1][0], BaseAngVel[1][1], BaseAngVel[1][2] ))
+      #print( "\n\nContact Points" ,ContactPoints if len(ContactPoints) > 0 else None )
+      # print("\nContactPoints (contactFlag, idA, idB, linkidA, linkidB, posA, posB, contactnormalonb, contactdistance, normalForce, lateralFriction1, lateralFrictionDir1, lateralFriction2, lateralFrictionDir2)\n\n".format(
+      # ))
+    # print("Contact Point ", len(ContactPoints), ContactPoints[0] if len(ContactPoints) > 0 else None )
 
-    # print("\nJointStates: (Pos,Vel,6 Forces [Fx, Fy, Fz, Mx, My, Mz], appliedJointMotorTorque)\n\n".format(
-
-    # ))
-    # print("\nBase Angular Velocity (Linear Vel(xyz) Algular Vel(wx,wy,wz)) \n\n".format(
-
-    # ))
-    # print("\ngetContactPoints (contactFlag, idA, idB, linkidA, linkidB, posA, posB, contactnormalonb, contactdistance, normalForce, lateralFriction1, lateralFrictionDir1, lateralFriction2, lateralFrictionDir2)\n\n".format(
-
-    # ))
-    return []
+    obs = np.array([
+      baseOri[0][2], #z (height) of the Torso -> 1
+      baseOri[1][0], #orientation (quarternion x,y,z,w) of the Torso -> 4
+      baseOri[1][1], 
+      baseOri[1][2], 
+      baseOri[1][3],
+      JointStates[0][0], # Joint angles(Pos) -> 8
+      JointStates[1][0], 
+      JointStates[2][0], 
+      JointStates[3][0], 
+      JointStates[4][0], 
+      JointStates[5][0], 
+      JointStates[6][0], 
+      JointStates[7][0], 
+      BaseAngVel[0][0],   #3-dim directional velocity and 3-dim angular velocity -> 3+3=6
+      BaseAngVel[0][1], 
+      BaseAngVel[0][2] , 
+      BaseAngVel[1][0], 
+      BaseAngVel[1][1], 
+      BaseAngVel[1][2],
+      JointStates[0][1], #Joint Velocities -> 8
+      JointStates[1][1], 
+      JointStates[2][1], 
+      JointStates[3][1], 
+      JointStates[4][1], 
+      JointStates[5][1], 
+      JointStates[6][1], 
+      JointStates[7][1]
+    ])
+    #External forces (force x,y,z + torque x,y,z) applied to the CoM of each link (Ant has 14 links: ground+torso+12(3links for 4legs) for legs -> (3+3)*(14)=84
+    external_forces = np.array([ np.array(joint[2]) for joint in JointStates ])
+    # print("Joint State Shape" , external_forces.shape, external_forces.flatten().shape )
+    obs = np.append( obs, external_forces.ravel() )
+    # print("Obs: ", obs.shape, obs)
+    return obs
 
   def compute_reward(self):
-    forward_reward = 0.0
-    contact_cost   = 0.0
-    ctrl_cost      = 0.0
+
+    baseOri   = np.array( p.getBasePositionAndOrientation(self.robotId) )
+    xposbefore = baseOri[0][0]
+    p.stepSimulation()
+    baseOri   = np.array( p.getBasePositionAndOrientation(self.robotId) )
+    xposafter = baseOri[0][0]
+    forward_reward = (xposafter - xposbefore)
+
+    JointStates   = p.getJointStates(self.robotId, self.movingJoints ) 
+    torques = np.array([ np.array(joint[3]) for joint in JointStates ])
+    ctrl_cost = .5 * np.square(torques).sum()
+
+    ContactPoints = p.getContactPoints(self.robotId, self.plane)
+    contact_cost = 0.5 * 1e-3 * len(ContactPoints)
     survive_reward = 1.0
     reward = forward_reward - ctrl_cost - contact_cost + survive_reward
+    # print("Reward ", reward , "Contact Cost ", contact_cost, "forward reward ",forward_reward, "Control Cost ", ctrl_cost)
+    print("Reward ", reward)
     return reward
 
   def compute_done(self):
@@ -139,7 +186,7 @@ if __name__ == "__main__":
     for i in range (max_steps):
       env.render()
       env.step(env.action_space.sample())
-      break
+      # break
     break
 
 
---
+++ robotschool_test.py
@@ -0,0 +1,34 @@
+import gym
+import roboschool
+
+from stable_baselines.common.policies import MlpPolicy
+from stable_baselines.common.vec_env import SubprocVecEnv
+from stable_baselines import PPO2
+
+# multiprocess environment
+n_cpu = 4
+env = SubprocVecEnv([lambda: gym.make('RoboschoolAnt-v1') for i in range(n_cpu)])
+# env = gym.make('RoboschoolAnt-v1')
+
+model = PPO2(MlpPolicy, env, verbose=1)
+# model.learn(total_timesteps=20000000)
+
+# model.save("ppo2_robotschool_ant_v2")
+# del model # remove to demonstrate saving and loading
+# model = PPO2.load("ppo2_robotschool_ant_v2")
+model = PPO2.load("ppo2_robotschool_ant_v2_2")
+
+# Enjoy trained agent
+obs = env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
+
+
+    
+
+# env.reset()
+# while True:
+#     env.step(env.action_space.sample())
+#     env.render()
